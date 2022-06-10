30f28716ea2afdbffc1ec3f15d5c9da69b3d8bc1
vleonecezar <vleone.social@gmail.com>
2022-06-10 00:11:30 -0300

    tipo enum

---
+++ src/009-tipo-enum/index.ts
@@ -0,0 +1,20 @@
+enum Cores {
+  VERMELHO,
+  AZUL,
+  AMARELO,
+}
+
+console.log(Cores[0]);
+console.log(Cores.VERMELHO);
+
+/* O TS faz um merge em 2 enums com mesmo nome */
+enum Cores {
+  ROXO = 3,
+  VERDE,
+}
+
+function escolhaCor(cor: Cores): void {
+  console.log(Cores[cor]);
+}
+
+escolhaCor(4);
